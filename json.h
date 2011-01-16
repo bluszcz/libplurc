47886a6302c730f0bea47175988cad8add72dcec
Guo-Fu Tseng <cooldavid@cooldavid.org>
2011-01-16 13:33:29 +0800

    Update license info

--- json.h
+++ json.h
@@ -1,3 +1,20 @@
+/*
+ * Json parser interface
+ *
+ * Copyright 2011 Guo-Fu Tseng <cooldavid@cooldavid.org>
+ *
+ * Author: Guo-Fu Tseng <cooldavid@cooldavid.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
 typedef struct json_parse_state {
 	const char *buf, *left;
 	union {
